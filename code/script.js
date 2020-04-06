a7f05b7d0cdf4120374db838bea921ae96bc5ff2
Sharmila <40612676+SharmilaSrinivasa@users.noreply.github.com>
2020-04-06 14:58:00 -0500

    Update script.js

--- code/script.js
+++ code/script.js
@@ -2,8 +2,6 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20, minPosition = 130, secPosition = 267;
-
 var date = new Date();
 console.log(date);
 
@@ -11,6 +9,12 @@ let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
 console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+
+
+let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12 );
+let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+let secPosition = sec * 360 / 60;
+
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
